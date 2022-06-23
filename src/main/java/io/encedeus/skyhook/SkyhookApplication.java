3702ee8247e60d6f86b1485e0cb9217f52e233df
BuildTools <unconfigured@null.spigotmc.org>
2022-06-23 21:03:10 +0200

    Added project setup

---
+++ src/main/java/io/encedeus/skyhook/SkyhookApplication.java
@@ -0,0 +1,13 @@
+package io.encedeus.skyhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyhookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyhookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/encedeus/skyhook/SkyhookApplicationTests.java
@@ -0,0 +1,13 @@
+package io.encedeus.skyhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyhookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
